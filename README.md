90e496bf0142c0f0f1a92ea08d72f557071a0cf7
0x6f677548 <64972114+0x6f677548@users.noreply.github.com>
2023-07-30 16:01:47 +0100

    refactor: changed copy approach to work on wsl environments

--- README.md
+++ README.md
@@ -3,3 +3,33 @@ Some utilities for the kobo eReader.
 
 ## change_user.py
 This script will help you change the user on your kobo eReader. When changing the user in a kobo device, you loose all your book status (read, unread, bookmarks, etc). This script will help you keep your book status and annotations.
+
+This is especially useful when you need to setup a new trial, and you want to keep your book status and annotations.
+
+### Usage
+```
+    python change_user.py <kobo_mount_point> <backup_destionation>
+```
+
+#### mounting kobo
+Make sure you have your kobo drive mounted. 
+If you are using wsl, you can mount your kobo with the following command:
+```
+    sudo mount -t drvfs <kobo_drive_letter> <kobo_mount_point>
+```
+Make sure you have your kobo mount point directory created. If you are using wsl, you can create it with the following command:
+```
+    sudo mkdir <kobo_mount_point>
+```
+Example:
+```
+    sudo mount -t drvfs e: /mnt/e
+```
+```
+    sudo mkdir /mnt/e
+```
+
+#### running the script
+```
+    python change_user.py /mnt/e /mnt/c/Users/username/Desktop/kobo_backup
+```
--- change_user.py
+++ change_user.py
@@ -10,14 +10,101 @@
 
 import argparse
 import os
-import shutil
 import datetime
 import os
 import sqlite3
+import fnmatch
+import subprocess
+
+#region copy helper functions
+
+def _copy_file(src, dst):
+    '''
+    Copies a file from src to dst
+    it uses the platform specific copy command, to avoid copying file permissions
+    '''
+    if os.name == "posix":  # Unix-based system (Linux, macOS)
+        subprocess.run(["cp", src, dst])
+    elif os.name == "nt":  # Windows
+        subprocess.run(["copy", src, dst], shell=True)
+    else:
+        raise Exception("Platform not supported. Unable to copy the file.")
 
 
+def _copy_tree(src, dest, ignore=None, symlink=False, force=False):
+
+    #  expand special characters in path
+    source_path = os.path.abspath(os.path.expanduser(src))
+    destination_path = os.path.abspath(os.path.expanduser(dest))
+
+    # Create destination path if it does not exists
+    if not os.path.exists(destination_path):
+        os.makedirs(destination_path)
+
+    # Get lists of all files and dirs in source path
+    source_items = os.listdir(source_path)
+
+    # Call ignore function and get the items to ignore
+    if ignore is not None:
+        ignored_names = ignore(source_path, source_items)
+    else:
+        ignored_names = set()
+
+
+    for item in source_items:
+
+        #filter ignored items
+        if item in ignored_names:
+            continue
+
+        source_item = os.path.join(source_path, item)
+        destination_item = os.path.join(destination_path, item)
+
+        # handle symlinks
+        if os.path.islink(source_item):
+            if symlink:
+                if os.path.lexists(destination_item):
+                    os.remove(destination_item)
+                os.symlink(os.readlink(source_item), destination_item)
+
+        # If source item is a directory, recursivly copy
+        elif os.path.isdir(source_item):
+            _copy_tree(source_item, destination_item, ignore, symlink, force)
+
+        # Skip if the file exists in the destination and force is False
+        elif os.path.isfile(destination_item):
+            if force:
+                print('Overwriting destination: {}'.format(repr(destination_item)))
+                _copy_file(source_item, destination_item)
+
+        # Copy other files
+        else:
+            print('Copying: {}'.format(repr(source_item)))
+            _copy_file(source_item, destination_item)
+
+def _ignore_patterns(*patterns):
+    """
+    List of patterns to ignore
+
+    :param args patterns: Defines a sequence of glob-style patterns
+                          to specify what files to ignore.
+    """
+    def __ignore_patterns(path, names):  # pylint: disable=unused-argument
+        "returns ignore list"
+
+        ignored_item = []
+        for pattern in patterns:
+            ignored_item.extend(fnmatch.filter(names, pattern))
+        return set(ignored_item)
+
+    return __ignore_patterns
+
+
+#endregion
 
 def _backup(args):
+    print("Backing up the Kobo device to the backup destination.. (this may take a while)")    
+
     
     # Ensure the Kobo mount point exists
     if not os.path.exists(args.kobo_mount_point):
@@ -29,18 +116,19 @@ def _backup(args):
         print(f"Error: Backup destination {args.backup_destination} exists. Please specify a backup destination that does not exist.")
         exit(1)
 
-    shutil.copytree(args.kobo_mount_point, args.backup_destination)
+    _copy_tree(args.kobo_mount_point, args.backup_destination)
     print(f"Backup complete. Files copied to {args.backup_destination}.")
 
 def _truncate_users_table(kobo_mount_point):
     print("We will now truncate the user table in the KoboReader.sqlite database")
     print("This will force the Kobo device to ask for the user account again")
-    input("Press Enter to continue...")
-
-
-    #open the KoboReader.sqlite database in the device
-    #and delete all the rows in the user table
-    #this will force the Kobo device to ask for the user account again
+    #ask the user to confirm
+    print("Are you sure you want to continue? (y/n)")
+    answer = input()
+    if answer != "y":
+        print("Aborting...")
+        exit(0)
+    
 
     #open the KoboReader.sqlite database in the device
     db = sqlite3.connect(kobo_mount_point + "/.kobo/KoboReader.sqlite")
@@ -56,11 +144,12 @@ def _truncate_users_table(kobo_mount_point):
     db.close()
 
 def _restore_backup(args):
-    print("Restoring the backup to the Kobo device..")
+    print("Restoring the backup to the Kobo device... (this may take a while)")
 
     #copy all the files from the backup device to the Kobo device except the .kobo folder
-    shutil.copytree(args.backup_destination, args.kobo_mount_point, dirs_exist_ok=True, ignore=shutil.ignore_patterns('.kobo'))
-    #os.system("rsync -av --delete --exclude=.kobo " + backup_mount_point + "/ " + kobo_mount_point + "/")
+    _copy_tree(args.backup_destination, args.kobo_mount_point, 
+                    force=True,
+                    ignore=_ignore_patterns('.kobo'))
 
     input("Restored. Press Enter to continue...")
 
@@ -74,14 +163,10 @@ def _create_temp_db(args):
     db_temp_filename = "KoboReader.sqlite" + ".temp-" + timestamp_str
 
     print("Doing a backup of the current device's KoboReader.sqlite database with the name " + db_device_backup_filename)
-
-    #use shutil.copy instead of os.system("cp") because os.system("cp") does not work on Windows
-    shutil.copy(args.kobo_mount_point + "/.kobo/KoboReader.sqlite", args.backup_destination + "/.kobo/" + db_device_backup_filename)
-    #os.system("cp " + args.kobo_mount_point + "/.kobo/KoboReader.sqlite " + args.backup_destination + "/.kobo/" + db_device_backup_filename)
+    _copy_file(args.kobo_mount_point + "/.kobo/KoboReader.sqlite", args.backup_destination + "/.kobo/" + db_device_backup_filename)
 
     print("Creating a temporary sqlite db file based on backup to manipulate the user table with the name " + db_temp_filename)
-    shutil.copy(args.backup_destination + "/.kobo/KoboReader.sqlite", args.backup_destination + "/.kobo/" + db_temp_filename)
-    #os.system("cp " + args.backup_destination + "/.kobo/KoboReader.sqlite " + args.backup_destination + "/.kobo/" + db_temp_filename)
+    _copy_file(args.backup_destination + "/.kobo/KoboReader.sqlite", args.backup_destination + "/.kobo/" + db_temp_filename)
 
     print("Press Enter to continue...")
 
@@ -115,6 +200,8 @@ def _create_temp_db(args):
     backup_db.close()
     return db_temp_filename
 
+    
+
 if __name__ == "__main__":
 
     # Create an argument parser to accept command line arguments
@@ -123,21 +210,60 @@ if __name__ == "__main__":
     parser.add_argument('backup_destination', type=str, help='Path to backup destination.')
     args = parser.parse_args()
 
-    _backup(args.kobo_mount_point, args.backup_destination)
+    #print the arguments
+    print("kobo_mount_point: " + args.kobo_mount_point)
+    print("backup_destination: " + args.backup_destination)
+
+    print("This script will backup the files from your Kobo device to a backup destination and then delete the user account from the Kobo device. " + 
+            "This will force the Kobo device to ask for the user account again and you can sign in with a different account. " +
+            "It will then restore the backup to the Kobo device.")
+
+    _backup(args)
     _truncate_users_table(args.kobo_mount_point)
 
     print ("Disconnect your Kobo device and sign in with the new account")
     print("Then reconnect your Kobo device and press Enter to continue...")
     input()
 
+    #test if the kobo mount point is valid
+    if not os.path.isdir(args.kobo_mount_point):
+        print("The Kobo mount point is not valid. Trying to mount it...")
+        os.system("mount " + args.kobo_mount_point)
+        if not os.path.isdir(args.kobo_mount_point):
+            
+            
+            #trying to infer the mount point from the mount point directory
+            # this is done by checking if the mount directory is a single letter drive and trying to find the drive letter
+            # ex: if the mount point is something like /mnt/k the drive will be k:
+            _mount_point_drive_letter = args.kobo_mount_point[-1] + ":"
+            _mount_command = "sudo mount -t drvfs " + _mount_point_drive_letter + " " + args.kobo_mount_point
+
+            print("The Kobo mount point is still not valid. If you are in a WSL environment, I can try to " + 
+                  "mount the drive with the following command: ")
+            print(_mount_command)
+            print("Does this makes sense at all to you? Do you want me to proceed? (y/n)")
+            _answer = input()
+            if _answer == "y":
+                os.system(_mount_command)
+            else:
+                exit()   
+            
+            if not os.path.isdir(args.kobo_mount_point):
+                print("The Kobo mount point is still not valid. Exiting...")
+                exit()
+            else:
+                print("The Kobo mount point is valid. Proceeding...")
+                print("Press Enter to continue...")
+                input()
+
+
+
     _restore_backup(args)
     db_temp_filename = _create_temp_db(args)
 
     #now let's copy the temp db to the device to replace the KoboReader.sqlite database that has the new user account
     print("Copying the temp db to the device")
-    shutil.copy(args.backup_destination + "/.kobo/" + db_temp_filename, args.kobo_mount_point + "/.kobo/KoboReader.sqlite")
-    #os.system("cp " + args.backup_destination + "/.kobo/" + db_temp_filename + " " + args.kobo_mount_point + "/.kobo/KoboReader.sqlite")
-
+    _copy_file(args.backup_destination + "/.kobo/" + db_temp_filename, args.kobo_mount_point + "/.kobo/KoboReader.sqlite")
 
     print("Done. You can now disconnect your Kobo device")
 
